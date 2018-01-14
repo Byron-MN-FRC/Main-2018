7d322bb73e4959205fafcdee42dc065ffd2ec772
robotics <robotics@HP-Elitebook-5>
2018-01-13 18:35:25 -0600

    added pneumatic class with shifting code

--- src/org/usfirst/frc/team4859/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Pneumatics.java
@@ -1,18 +1,34 @@
 package org.usfirst.frc.team4859.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Pneumatics extends Subsystem {
+	DoubleSolenoid shifterLeft = new DoubleSolenoid(0, 1);
+	DoubleSolenoid shifterRight = new DoubleSolenoid(2, 3);
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+    public void initDefaultCommand() {}
+    
+    public void pneumaticShiftUp(){
+    	
+    	shifterLeft.set(Value.kForward);
+    	shifterRight.set(Value.kForward);
+    	
     }
+    
+    public void pneumaticShiftDown(){
+    	
+    	shifterLeft.set(Value.kReverse);
+    	shifterRight.set(Value.kReverse);
+    	
+    }
+    
 }
 
