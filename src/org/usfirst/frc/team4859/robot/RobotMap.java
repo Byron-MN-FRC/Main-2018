a0da0f8a51aa7e4a81d4bf61a8b341d21693dab1
chaube97 <chaube97@students.byron.k12.mn.us>
2018-02-03 21:30:11 -0600

    Added turn right and left 90 deg buttons and added current limiting

--- src/org/usfirst/frc/team4859/robot/RobotMap.java
+++ src/org/usfirst/frc/team4859/robot/RobotMap.java
@@ -26,18 +26,24 @@ public class RobotMap {
 	public static double liftScaleHeight = 0.5;
 	public static double liftClimbHeight = 0.5;
 	
+	// Current limiting
+	public static int kContinuousCurrentLimit = 16; // Amps
+	public static int kCurrentPeakDuration = 1000; // Milliseconds
+	public static int kCurrentPeakLimit = 20; // Amps
+	
 	// Closed loop values
 	public static int kTimeoutMs = 10;
 	public static int kPIDSlot = 0;
 	
+	// Drivetrain values
 	public static double kP = 0.22;
 	public static double kI = 0.0015;
 	public static double kD = 0.000;
-	public static double kF = 0.085;
+	public static double kF = 0.09;
 	public static int kAllowableError = 90;
 	
-	public static int kHighGearAcceleration = 4500;
-	public static int kHighGearCruiseVelocity = 6000;
+	public static int kHighGearAcceleration = 9000;
+	public static int kHighGearCruiseVelocity = 9000;
 	
 	public static int kLowGearAcceleration = 2720;
 	public static int kLowGearCruiseVelocity = 2720;
@@ -46,9 +52,9 @@ public class RobotMap {
 	public static int kLifterCruiseVelocity = 2720;
 	
 	// Robot numbers for closed loop (in inches)
-	public static double robotWidth = 25.5;
+	public static double robotWidth = 27;
 
-	//			   				wheel diameter * pi / encoder units per revolution / 2 (sprocket 2:1 reduction)
+	//			   				wheel diameter * pi / encoder units per revolution / 2 (sprocket 2:1 reduction) = 1264.77
 	public static double encoderUnitsPerInch = 1 / (6 * Math.PI) * 4096 * 2 * 2.9102;
 	
 	// Precision mode
