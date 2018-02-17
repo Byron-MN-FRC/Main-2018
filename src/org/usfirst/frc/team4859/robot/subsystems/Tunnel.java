4c0bf8cc0f41809490a1a967432866cb78163484
chaube97 <chaube97@students.byron.k12.mn.us>
2018-02-17 16:05:53 -0600

    Added the top motor for the box

--- src/org/usfirst/frc/team4859/robot/subsystems/Tunnel.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Tunnel.java
@@ -8,21 +8,25 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Tunnel extends Subsystem {
 	//public static Talon motorTunnelLeft = new Talon(RobotMap.talonIDTunnelLeft);
 	//public static Talon motorTunnelRight = new Talon(RobotMap.talonIDTunnelRight);
+	//public static Talon motorTunnelTop = new Talon(RobotMap.talonIDTunnelTop);
 
     public void initDefaultCommand() {}
     
     public void tunnelIntake(double inputSpeed) {
     	//motorTunnelLeft.set(-inputSpeed);
-    	//motorTunnelLeft.set(inputSpeed);
+    	//motorTunnelRight.set(inputSpeed);
+    	//motorTunnelTop.set(inputSpeed);
     }
     
     public void tunnelShoot(double inputSpeed) {
     	//motorTunnelLeft.set(inputSpeed);
-    	//motorTunnelLeft.set(-inputSpeed);
+    	//motorTunnelRight.set(-inputSpeed);
+    	//motorTunnelTop.set(-inputSpeed);
     }
     
     public void tunnelStop() {
     	//motorTunnelLeft.set(0);
     	//motorTunnelLeft.set(0);
+    	//motorTunnelTop.set(0);
     }  
 }
