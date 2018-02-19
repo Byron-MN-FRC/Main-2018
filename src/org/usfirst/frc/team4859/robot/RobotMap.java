fabae541f34ae4797b9f04817c6381c07b1407f6
ecockr38 <ecockr38@students.byron.k12.mn.us>
2018-02-19 13:01:04 -0600

    forgot to delete motors

--- src/org/usfirst/frc/team4859/robot/RobotMap.java
+++ src/org/usfirst/frc/team4859/robot/RobotMap.java
@@ -17,9 +17,6 @@ public class RobotMap {
 	public static int talonIDLiftStage1 = 7;
 	public static int talonIDLiftStage2 = 8;
 	
-	public static int talonIDAcquireLeft = 0;
-	public static int talonIDAcquireRight = 5;
-	
 	public static int talonIDTunnelLeft = 1;
 	public static int talonIDTunnelRight = 2;
 	public static int talonIDTunnelTop = 3;
